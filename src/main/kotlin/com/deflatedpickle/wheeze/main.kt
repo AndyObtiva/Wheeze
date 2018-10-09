9c38a161def8f3156baa99c9b96130e3efb40fc4
DeflatedPickle <DeflatedPickle@gmail.com>
2018-10-09 11:12:59 +0100

    Added the window

---
+++ src/main/kotlin/com/deflatedpickle/wheeze/main.kt
@@ -0,0 +1,32 @@
+package com.deflatedpickle.wheeze
+
+import com.deflatedpickle.wheeze.canvas.PaintCanvas
+import org.eclipse.swt.SWT
+import org.eclipse.swt.custom.SashForm
+import org.eclipse.swt.layout.FillLayout
+import org.eclipse.swt.layout.GridLayout
+import org.eclipse.swt.widgets.Display
+import org.eclipse.swt.widgets.Shell
+
+fun main(args: Array<String>) {
+    val display = Display()
+    val shell = Shell(display)
+    shell.text = "Wheeze"
+
+    val layout = FillLayout()
+    shell.layout = layout
+
+    val horizontalForm  = SashForm(shell, SWT.HORIZONTAL or SWT.SMOOTH)
+
+    horizontalForm.pack()
+
+    shell.pack()
+    shell.open()
+
+    while (!shell.isDisposed) {
+        if (!display.readAndDispatch()) {
+            display.sleep()
+        }
+    }
+    display.dispose()
+}
